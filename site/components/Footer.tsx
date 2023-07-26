4a9ceae6305c24ae391741f8af8ca4456416a0cb
Tapas Adhikary <tapas.adhikary@gmail.com>
2023-07-26 17:37:23 +0530

    Added a navbar and footer component

---
+++ site/components/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 py-2">
+      <div className="container mx-auto px-4 flex justify-between items-center text
+      white font-bold uppercase tracking-widest">
+        © 2023, Built with Next.js App Router and TailwindCSS. All rights
+        reserved.
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ site/components/Navbar.tsx
@@ -0,0 +1,57 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="flex items-center justify-between flex-wrap bg-blue-600 p-6">
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <Link href="/" className="font-semibold text-xl tracking-tight">
+          Coursey
+        </Link>
+      </div>
+      <div className="block lg:hidden">
+        <button className="flex items-center px-3 py-2 border rounded text-blue-200 border-blue-400 hover:text-white hover:border-white">
+          <svg
+            className="fill-current h-3 w-3"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          </svg>
+        </button>
+      </div>
+      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div className="text-sm lg:flex-grow">
+          <a
+            href="https://youtube.com/@tapasadhikary"
+            className="block mt-4 lg:inline-block lg:mt-0 text-blue-200 hover:text-white mr-4"
+          >
+            Join tapaScript
+          </a>
+          <a
+            href="https://blog.greenroots.info"
+            className="block mt-4 lg:inline-block lg:mt-0 text-blue-200 hover:text-white mr-4"
+          >
+            My Blog
+          </a>
+          <a
+            href="https://tapasadhikary.com"
+            className="block mt-4 lg:inline-block lg:mt-0 text-blue-200 hover:text-white"
+          >
+            My Talks
+          </a>
+        </div>
+        <div>
+          <a
+            href="https://twitter.com/tapasadhikary"
+            className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 lg:mt-0"
+          >
+            Follow on X
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
